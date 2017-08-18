147796b6e2834156f90dc2c9331cbadc20943189
Daniel Oduonye <contactdanny19@gmail.com>
2017-08-18 02:45:14 +0100

    add response map

--- README.md
+++ README.md
@@ -1,57 +1,16 @@
 
 
-A simple bot for reporting things like pot holes, absent students and cranky waiters for greenbot. 
+A simple bot that analyses the emotional state of the customer 
 
 ## Conversation Flow
 
 1. This bot starts when someone sends it a message. 
 2. The bot sends a greeting, should be something like "Welcome to the Barnstable DPW pot hole bot."
-3. The bot sends a question to be answered, like. "Please tell us all about your pothole."
-4. The bot starts listening for messages. After every message, it asks for more. 
-5. Once the texter sends a single q, note taking mode will end.
-6. The bot will then ask the texter if they want to be contacted directly. This behavior cannot be disabled. 
-7. If they want to be contacted, the bot asks for who to ask for, and if there is another number to try.  
-8. The conversation ends with two more prompts. 
-  
-
-```
-#!/usr/bin/env ruby
-
-```
-
-
-
-
-
-
-
-
-
-
+3. The bot starts listening for messages. After every message, it asks for more. 
+4. Once the texter sends a single q, note taking mode will end.
+6. The bot will predicts the emotional state of the customer and responds with a corresponding text   
 
   
-
-```
-require "./lib/greenbot.rb"
-tell ENV['PROMPT_1']
-issue = note(ENV['PROMPT_2'])
-if confirm("Would you like someone to contact you?")
-  contact_me = true
-  contact_me.remember("contact_me")
-  name = ask("When we call, who should we ask for?")
-  name.remember("who_to_ask_for")
-  if confirm("Is there another number we should try?")
-    better_number = ask("Please enter that number with an area code")
-    better_number.remember("better_number")
-  end
-else
-  tell("No problem at all.")
-end
-tell ENV['PROMPT_3']
-tell ENV['SIGNATURE']
-
-
-```
 
 
 
--- bot.rb
+++ bot.rb
@@ -1,20 +1,60 @@
 #!/usr/bin/env ruby
 #
 #
-require "./lib/greenbot.rb"
-tell ENV['PROMPT_1']
-issue = note(ENV['PROMPT_2'])
-if confirm("Would you like someone to contact you?")
-  contact_me = true
-  contact_me.remember("contact_me")
-  name = ask("When we call, who should we ask for?")
-  name.remember("who_to_ask_for")
-  if confirm("Is there another number we should try?")
-    better_number = ask("Please enter that number with an area code")
-    better_number.remember("better_number")
-  end
-else
-  tell("No problem at all.")
+require 'uri'
+require 'net/http'
+require 'json'
+require './lib/greenbot'
+
+def analyse_sentiment(text)
+  url = URI("https://tone-analyzer-demo.mybluemix.net/api/tone")
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Post.new(url)
+  request["content-type"] = 'application/x-www-form-urlencoded'
+  request["cache-control"] = 'no-cache'
+  request.body = "text=#{text}"
+  response = http.request(request)
+  parse_response(response.read_body)
 end
-tell ENV['PROMPT_3']
-tell ENV['SIGNATURE']
+
+def get_reply(tone)
+  response_map = Hash[
+      "sadness" => ENV['SAD_MESSAGE'] || "I really understand how you feel",
+      "joy" => ENV['JOY_MESSAGE'] || "Great, someone seems to be in a good mood",
+      "disgust" => ENV['DISGUST_MESSAGE'] || "I understand how frustrating this might seem",
+      "anger" => ENV['ANGER_MESSAGE'] || "Kindly be calm, we will resolve this I assure you",
+      "fear" => ENV['FEAR_MESSAGE'] || "It is not as bad as it seems. Everything will be alright",
+  ]
+  return response_map[tone]
+end
+
+
+def parse_response(response)
+  response = JSON.parse(response)
+  emotional_state_index = 0
+  emotional_tone_list = response['document_tone']['tone_categories'][emotional_state_index]['tones'];
+  current_emotional_tone = emotional_tone_list[0];
+  $i = 1
+  while $i < emotional_tone_list.length  do
+    if emotional_tone_list[$i]['score'] > current_emotional_tone['score']
+      current_emotional_tone = emotional_tone_list[$i]
+    end
+    $i += 1
+  end
+
+  puts get_reply(current_emotional_tone['tone_id'])
+end
+
+
+
+PROMPT_1 = ENV['PROMPT_1'] || "How can we help you today?"
+issue = note(PROMPT_1)
+
+analyse_sentiment(issue)
+
+
+
