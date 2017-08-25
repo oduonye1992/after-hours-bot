f690a3245b2b89b81019d2e53459a34903d52dde
Daniel Oduonye <contactdanny19@gmail.com>
2017-08-25 06:13:58 +0100

    Initial commit - Seems to work

--- bot.rb
+++ bot.rb
@@ -2,100 +2,38 @@
 #
 #
 require 'uri'
-require 'net/http'
 require 'json'
 require './lib/greenbot'
+require 'date'
 
-require 'mail'
-
-Mail.defaults do
-  delivery_method :smtp, { :address   => ENV['MAIL_ADDRESS'],
-                           :port      => ENV['MAIL_PORT'],
-                           :user_name => ENV['MAIL_USERNAME'],
-                           :domain    => ENV['MAIL_DOMAIN'],
-                           :password  => ENV['MAIL_PASSWORD'], 
-                           :authentication => 'plain',
-                           :enable_starttls_auto => true }
+WEEKDAY_CLOSING_TIME = ENV['WEEKDAY_CLOSING_TIME'] || "07:00" 
+WEEKEND_CLOSING_TIME = ENV['WEEKEND_CLOSING_TIME'] || "17:00"  
+OFFICE_CLOSED_COMMENT = ENV['OFFICE_CLOSED_COMMENT'] || "Sorry, We are closed for the day"
+NO_WEEKEND_HOURS = "none"
+def isWeekday()
+    today = Date.today
+    return today.wday <= 5
 end
 
-
-def analyse_sentiment(text)
-  url = URI("https://tone-analyzer-demo.mybluemix.net/api/tone")
-
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  request = Net::HTTP::Post.new(url)
-  request["content-type"] = 'application/x-www-form-urlencoded'
-  request["cache-control"] = 'no-cache'
-  request.body = "text=#{text}"
-  response = http.request(request)
-  return parse_response(response.read_body)
+def getFormattedTime(timeStr)
+    currentTime = timeStr
+    currentTime = currentTime.gsub(':', '')
+    currentTime = currentTime.to_i
 end
 
-def angry?(messages)
- tone = analyse_sentiment(messages)
- if tone['tone_id'] == "anger"
-   return true
- else 
-   return false
- end
-end
-
-
-def parse_response(response)
-  puts response
-  response = JSON.parse(response)
-  emotional_state_index = 0
-  emotional_tone_list = response['document_tone']['tone_categories'][emotional_state_index]['tones'];
-  current_emotional_tone = emotional_tone_list[0];
-  $i = 1
-  while $i < emotional_tone_list.length  do
-    if emotional_tone_list[$i]['score'] > current_emotional_tone['score']
-      current_emotional_tone = emotional_tone_list[$i]
+def isAfterWorkHours()
+    if (!isWeekday())
+        tmpStr = WEEKEND_CLOING_TIME.downcase
+        if (tmpStr === NO_WEEKEND_HOURS)
+            puts OFFICE_CLOSED_COMMENT
+        end
     end
-    $i += 1
-  end
-  return current_emotional_tone
+    currentTime = getFormattedTime(Time.now.strftime("%H:%M"))
+    officeClosingTime = getFormattedTime(isWeekday() ? WEEKDAY_CLOSING_TIME : WEEKEND_CLOSING_TIME)
+    return currentTime > officeClosingTime
 end
 
-# puts get_reply(current_emotional_tone['tone_id'])
-
-
-#PROMPT_1 = ENV['PROMPT_1'] || "How can we help you today?"
-#issue = note(PROMPT_1)
-
-def send_notification(messages, email)
-  puts "Someone is angry"
-  email = EMAILS[0]
-  _from, _to = 'bot@green-bot.com', email
-
-  Mail.deliver do
-    to _to
-    from _from
-    subject 'The customer seems angry based on the messages bellow'
-    text_part do
-      body messages
-    end
-  end
-end
-
-
-#analyse_sentiment(issue)
-messages = ""
-
-THRESHOLD = ENV['ANALYSIS_THRESHOLD'].to_i || 50
-EMAILS = ENV['EMAILS'].split(",")
-email = EMAILS[0]
-
-while true
-  message = gets
-  messages += message
-  if(messages.length >= THRESHOLD)
-    puts "message length now #{messages.length}"
-    send_notification(messages, email) if angry?(messages)
-    messages = ""
-  end
+if (isAfterWorkHours())
+     puts OFFICE_CLOSED_COMMENT
 end
 
