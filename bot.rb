386aee54eaeea2e314fd376db6a1d1c8b80ddf7c
Daniel Oduonye <contactdanny19@gmail.com>
2017-08-28 07:11:42 +0100

    Add support for timezones

--- bot.rb
+++ bot.rb
@@ -6,10 +6,12 @@ require 'json'
 require './lib/greenbot'
 require 'date'
 
-WEEKDAY_CLOSING_TIME = ENV['WEEKDAY_CLOSING_TIME'] || "07:00" 
-WEEKEND_CLOSING_TIME = ENV['WEEKEND_CLOSING_TIME'] || "17:00"  
+WEEKDAY_CLOSING_TIME = ENV['WEEKDAY_CLOSING_TIME'] || "07:00"
+WEEKEND_CLOSING_TIME = ENV['WEEKEND_CLOSING_TIME'] || "17:00"
+OFFICE_TIMEZONE = ENV['OFFICE_TIMEZONE'] || "+0:00"
 OFFICE_CLOSED_COMMENT = ENV['OFFICE_CLOSED_COMMENT'] || "Sorry, We are closed for the day"
 NO_WEEKEND_HOURS = "none"
+
 def isWeekday()
     today = Date.today
     return today.wday <= 5
@@ -17,19 +19,31 @@ end
 
 def getFormattedTime(timeStr)
     currentTime = timeStr
-    currentTime = currentTime.gsub(':', '')
-    currentTime = currentTime.to_i
+    currentTime = currentTime.split(':')
+    return (currentTime[0].to_i * 60) + currentTime[1].to_i
+end
+
+def getFormattedTimeWithTimezone(time, timezone)
+    operation = timezone[0]
+    if (operation != "+" || operation != "-")
+        return getFormattedTime(time)
+    end
+    timezone[0] = ''
+    timezone = getFormattedTime(timezone)
+    time = getFormattedTime(time)
+    return operation == "+" ? time + timezone : time - timezone
 end
 
 def isAfterWorkHours()
     if (!isWeekday())
-        tmpStr = WEEKEND_CLOING_TIME.downcase
+        tmpStr = WEEKEND_CLOSING_TIME.downcase
         if (tmpStr === NO_WEEKEND_HOURS)
-            puts OFFICE_CLOSED_COMMENT
+            return true
         end
     end
-    currentTime = getFormattedTime(Time.now.strftime("%H:%M"))
-    officeClosingTime = getFormattedTime(isWeekday() ? WEEKDAY_CLOSING_TIME : WEEKEND_CLOSING_TIME)
+    currentTime = getFormattedTime(Time.now.utc.strftime("%H:%M"))
+    officeTime = isWeekday() ? WEEKDAY_CLOSING_TIME : WEEKEND_CLOSING_TIME;
+    officeClosingTime = getFormattedTimeWithTimezone(officeTime, OFFICE_TIMEZONE)
     return currentTime > officeClosingTime
 end
 
